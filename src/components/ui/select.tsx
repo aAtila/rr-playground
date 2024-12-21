12c4cd09a57621a5fa10fb2c818ed5f9254e1f6e
Atila Alacan <alacanatila@gmail.com>
2024-12-21 16:40:02 +0100

    add `className` prop support to `Select` component

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,9 +1,14 @@
 import { SelectHTMLAttributes } from "react";
+import { cn } from "../../lib/utils";
 
 export default function Select({
+  className,
   ...props
 }: SelectHTMLAttributes<HTMLSelectElement>) {
   return (
-    <select className="border border-gray-300 rounded-md p-2" {...props} />
+    <select
+      className={cn("border border-gray-300 rounded-md p-2", className)}
+      {...props}
+    />
   );
 }
